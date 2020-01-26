ead412f6ba7ff2f452b46a1a906c6c26e1ee7286
Peter Tardif <peter.tardif@gmail.com>
2020-01-26 14:42:08 -0700

    finalized GCF

--- greatestCommonDenominator.js
+++ greatestCommonDenominator.js
@@ -3,22 +3,21 @@
 // For example, given the numbers [42, 56, 14], return 14.
 
 function greatestCommonDenominator(array) {
-  if ( min === 1) {
-    return 1; 
-  }
-  // sort the array
-  let sortedArray = array.sort();
-  // Find the min
-  let min = sortedArray[0];
-
-  // check if all numbers are divisible by min, if true return min
-  if (sortedArray.every(arrItem => arrItem % min === 0)) {
-    return min;
-  } 
-  // else subtract 1 from the min
-  else {
-    min - 1
-
-  }
-  // check if new min is visible by array, if true return, else repeat
+	// sort the array
+	let sortedArray = array.sort();
+	// Find the min
+	let min = sortedArray[0];
+	// check if all numbers are divisible by min, if true return min
+	while (min > 1) {
+		if (sortedArray.every(arrItem => arrItem % min === 0)) {
+			return min;
+		}
+		// else subtract 1 from the min
+		min--;
+	}
+	// if the min gets down to 2, we breakout and return the min which is now 1 because this will always be the greatest common denominator for whole numbers.
+	return min;
 }
+
+const numbers = [10, 4, 120];
+console.log(greatestCommonDenominator(numbers));
